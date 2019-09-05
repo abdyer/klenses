5d481e5ff8be3e75cca1ae92db9b51566d38003c
Andy Dyer <andrew.burton.dyer@zalando.de>
2019-09-05 11:56:24 +0200

    Update to Kotlin 1.3.21

--- src/main/kotlin/com/codepoetics/klenses/Lens.kt
+++ src/main/kotlin/com/codepoetics/klenses/Lens.kt
@@ -24,7 +24,7 @@ interface Lens<T, V> {
     fun set(t: T, v: V): T
     fun update(t: T, update: V.() -> V): T = set(t, get(t).update())
 
-    operator fun <V2> mod(iso: Iso<V, V2>): Lens<T, V2> = Lens.of(
+    operator fun <V2> rem(iso: Iso<V, V2>): Lens<T, V2> = of(
             { t -> iso.arrow(get(t)) },
             { t, v2 -> set(t, iso.opArrow(v2)) }
     )
@@ -33,7 +33,7 @@ interface Lens<T, V> {
     operator fun invoke(t: T, v: V): T = set(t, v)
     operator fun invoke(t: T, update: V.() -> V): T = update(t, update)
 
-    operator fun <V1> plus(next: Lens<V, V1>): Lens<T, V1> = Lens.of(
+    operator fun <V1> plus(next: Lens<V, V1>): Lens<T, V1> = of(
             { t -> next.get(get(t)) },
             { t, v -> set(t, next.set(get(t), v)) }
     )
@@ -68,7 +68,7 @@ infix fun <T, V> Lens<T, V?>.orElse(default: V): Lens<T, V> = Lens.of(
 inline infix fun <reified T : Any, V> KProperty1<T, V?>.orElse(default: V): Lens<T, V> = this.lens() orElse default
 
 // Modulo for property
-inline operator fun <reified T : Any, V, V1> KProperty1<T, V>.mod(iso: Iso<V, V1>): Lens<T, V1> = this.lens() % iso
+inline operator fun <reified T : Any, V, V1> KProperty1<T, V>.rem(iso: Iso<V, V1>): Lens<T, V1> = this.lens() % iso
 
 // Property.get
 inline fun <reified T : Any, V> KProperty1<T, V>.get(t: T): V = this.lens().get(t)
@@ -83,24 +83,24 @@ inline fun <reified T : Any, V> KProperty1<T, V>.update(t: T, noinline update: V
 inline operator fun <reified T : Any, V> KProperty1<T, V>.invoke(t: T, noinline update: V.() -> V): T = this.lens()(t, update)
 
 class PropertyMapper<T : Any>(
-        val constructor: KFunction<T>,
-        val members: Array<KCallable<*>>,
-        val updatableMembers: Set<KCallable<*>>) {
+        private val constructor: KFunction<T>,
+        private val members: Array<KProperty1<out Any?, Any?>>,
+        private val updatableMembers: Set<KCallable<*>>) {
 
     companion object {
         private val cache: ConcurrentMap<KClass<*>, PropertyMapper<*>> = ConcurrentHashMap()
 
         @Suppress("UNCHECKED_CAST")
         fun <T : Any> forKClass(kclass: KClass<T>): PropertyMapper<T> =
-            cache.computeIfAbsent(kclass, { forKClassUncached(it) }) as PropertyMapper<T>
+            cache.computeIfAbsent(kclass) { forKClassUncached(it) } as PropertyMapper<T>
 
         private fun <T : Any >forKClassUncached(kclass: KClass<T>): PropertyMapper<T> {
-            val propertiesByName = kclass.members.filter { it is KProperty1<*, *> }
+            val propertiesByName = kclass.members.filterIsInstance<KProperty1<*, *>>()
                 .map { it.name to it }
                 .toMap()
             val propertyMappableConstructors = kclass.constructors.filter {
                 propertiesByName.keys.containsAll(it.parameters.map { it.name!! }) }
-            val constructor = propertyMappableConstructors.sortedBy { 0 - it.parameters.size }.first()
+            val constructor = propertyMappableConstructors.minBy { 0 - it.parameters.size }!!
             val members = constructor.parameters.map { propertiesByName[it.name!!]!! }.toTypedArray()
             return PropertyMapper(constructor, members, members.toHashSet())
         }
